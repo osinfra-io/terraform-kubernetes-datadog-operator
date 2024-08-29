13a20faefb5071e36144c9f92cb61de0d6ae748d
Brett Curtis <brett@osinfra.io>
2024-08-29 19:02:21 -0400

    Ignoring unwanted resources in APM

--- regional/manifests/variables.tf
+++ regional/manifests/variables.tf
@@ -144,7 +144,7 @@ variable "node_agent_env_vars" {
   default = [
     {
       name  = "DD_APM_FILTER_TAGS_REJECT"
-      value = "[{\"http.useragent\":\"kube-probe\\/\\d+\\.\\d+\"}]"
+      value = "http.useragent:kube-probe/1.30"
     },
     {
       name  = "DD_CONTAINER_EXCLUDE"
