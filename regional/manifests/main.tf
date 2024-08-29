6b3908e2a60c26635490b864d641bb5fcecb18ec
Brett Curtis <brett@osinfra.io>
2024-08-29 19:24:55 -0400

    Ignoring unwanted resources in APM

--- regional/manifests/main.tf
+++ regional/manifests/main.tf
@@ -99,6 +99,17 @@ resource "kubernetes_manifest" "agent" {
                 }
               }
             }
+
+            trace-agent = {
+              env = {
+
+                # Ignoring Unwanted Resources in APM
+                # https://docs.datadoghq.com/tracing/guide/ignoring_apm_resources
+
+                name  = "DD_APM_FILTER_TAGS_REJECT"
+                value = "http.useragent:kube-probe/1.30"
+              }
+            }
           }
 
           env = var.node_agent_env_vars
@@ -132,17 +143,6 @@ resource "kubernetes_manifest" "agent" {
 
           priorityClassName = kubernetes_priority_class_v1.datadog.metadata.0.name
           tolerations       = var.node_agent_tolerations
-        }
-
-        traceAgent = {
-          env = {
-
-            # Ignoring Unwanted Resources in APM
-            # https://docs.datadoghq.com/tracing/guide/ignoring_apm_resources
-
-            name  = "DD_APM_FILTER_TAGS_REJECT"
-            value = "http.useragent:kube-probe/1.30"
-          }
         }
       }
     }
