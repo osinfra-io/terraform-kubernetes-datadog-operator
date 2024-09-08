0092d4aadaf676d2264b925a1e0b67f60732314b
Brett Curtis <brett@osinfra.io>
2024-09-08 15:59:54 -0400

    Cleanup monitor names and messages

--- regional/manifests/locals.tf
+++ regional/manifests/locals.tf
@@ -7,10 +7,10 @@ locals {
   kubernetes_monitor_templates = {
     "crash-loop-backoff" = {
       message = <<-EOF
-      Pod {{pod_name.name}} is in CrashLoopBackOff on {{kube_namespace.name}} on {{kube_cluster_name.name}} cluster.
+      Pod {{pod_name.name}} is in CrashLoopBackOff in {{kube_namespace.name}} on cluster: {{kube_cluster_name.name}}.
       EOF
 
-      name                = "[Kubernetes] Pods in CrashLoopBackOff on namespace {{kube_namespace.name}} on ${local.kubernetes_cluster_name}"
+      name                = "[Kubernetes] Pods in CrashLoopBackOff - namespace: {{kube_namespace.name}} cluster: ${local.kubernetes_cluster_name}"
       priority            = 3
       query               = "max(last_10m):default_zero(max:kubernetes_state.container.status_report.count.waiting{reason:crashloopbackoff, kube_cluster_name:${local.kubernetes_cluster_name}} by {kube_namespace,pod_name}) >= 1"
       thresholds_critical = 1
@@ -20,10 +20,10 @@ locals {
 
     "failing-deployment-replicas" = {
       message = <<-EOF
-      More than one Deployments Replica pods are down in Deployment {{kube_namespace.name}}/{{kube_deployment.name}} on {{kube_cluster_name.name}} cluster."\
+      More than one Deployments Replica pods are down in Deployment {{kube_namespace.name}}/{{kube_deployment.name}} on cluster: {{kube_cluster_name.name}}."
       EOF
 
-      name                = "[Kubernetes] Deployments Replica Pods are down on ${local.kubernetes_cluster_name}"
+      name                = "[Kubernetes] Deployments Replica Pods are down - cluster: ${local.kubernetes_cluster_name}"
       priority            = 3
       query               = "avg(last_15m):avg:kubernetes_state.deployment.replicas_desired{kube_cluster_name:${local.kubernetes_cluster_name}} by {kube_namespace,kube_deployment} - avg:kubernetes_state.deployment.replicas_available{kube_cluster_name:${local.kubernetes_cluster_name}} by {kube_namespace,kube_deployment} >= 2"
       thresholds_critical = 2
@@ -33,10 +33,10 @@ locals {
 
     "failing-pods" = {
       message = <<-EOF
-      More than ten pods are failing in ({{kube_cluster_name.name}} cluster).
+      More than ten pods are failing on cluster: {{kube_cluster_name.name}}.
       EOF
 
-      name                = "[Kubernetes] Failed Pods in Namespaces on ${local.kubernetes_cluster_name}"
+      name                = "[Kubernetes] Failed Pods in Namespaces - cluster: ${local.kubernetes_cluster_name}"
       priority            = 3
       query               = "change(avg(last_5m),last_5m):default_zero(sum:kubernetes_state.pod.status_phase{pod_phase:failed, kube_cluster_name:${local.kubernetes_cluster_name}} by {kube_namespace,pod_name}) > 10"
       thresholds_critical = 10
@@ -46,10 +46,10 @@ locals {
 
     "failing-statefulset-replicas" = {
       message = <<-EOF
-      More than one Statefulset Replica pods are down in Statefulset {{kube_namespace.name}}/{{kube_stateful_set.name}} on {{kube_cluster_name.name}} cluster.
+      More than one Statefulset Replica pods are down in Statefulset {{kube_namespace.name}}/{{kube_stateful_set.name}} on cluster: {{kube_cluster_name.name}}.
       EOF
 
-      name                = "[Kubernetes] Statefulset Replicas are down on ${local.kubernetes_cluster_name}"
+      name                = "[Kubernetes] Statefulset Replicas are down - cluster: ${local.kubernetes_cluster_name}"
       priority            = 3
       query               = "max(last_15m):sum:kubernetes_state.statefulset.replicas_desired{kube_cluster_name:${local.kubernetes_cluster_name}} by {kube_namespace,kube_stateful_set} - sum:kubernetes_state.statefulset.replicas_ready{kube_cluster_name:${local.kubernetes_cluster_name}} by {kube_namespace,kube_stateful_set} >= 2"
       thresholds_critical = 2
@@ -59,10 +59,10 @@ locals {
 
     "high-cpu-usage" = {
       message = <<-EOF
-      CPU usage is over 90% on {{kube_namespace.name}}/{{kube_pod_name.name}} on {{kube_cluster_name.name}} cluster.
+      Pod CPU usage is over 90% on {{kube_namespace.name}}/{{kube_pod_name.name}} on cluster: {{kube_cluster_name.name}}.
       EOF
 
-      name                = "[Kubernetes] Pod CPU usage is high on ${local.kubernetes_cluster_name}"
+      name                = "[Kubernetes] Pod CPU usage is high - cluster: ${local.kubernetes_cluster_name}"
       priority            = 3
       query               = "avg(last_5m):(avg:kubernetes.cpu.usage.total{kube_cluster_name:${local.kubernetes_cluster_name}} by {kube_namespace,pod_name} / 10000000) / avg:kubernetes.cpu.limits{kube_cluster_name:${local.kubernetes_cluster_name}} by {kube_namespace,pod_name} > 90"
       thresholds_critical = 90
@@ -72,10 +72,10 @@ locals {
 
     "high-memory-usage" = {
       message = <<-EOF
-      Memory usage is over 90% on {{kube_namespace.name}}/{{kube_pod_name.name}} on {{kube_cluster_name.name}} cluster.
+      Pod memory usage is over 90% on {{kube_namespace.name}}/{{kube_pod_name.name}} on cluster: {{kube_cluster_name.name}}.
       EOF
 
-      name                = "[Kubernetes] Pod Memory usage is high on ${local.kubernetes_cluster_name}"
+      name                = "[Kubernetes] Pod memory usage is high - cluster: ${local.kubernetes_cluster_name}"
       priority            = 3
       query               = "avg(last_5m):avg:kubernetes.memory.usage{kube_cluster_name:${local.kubernetes_cluster_name}} by {kube_namespace,pod_name} / avg:kubernetes.memory.limits{kube_cluster_name:${local.kubernetes_cluster_name}} by {kube_namespace,pod_name} * 100 > 90"
       thresholds_critical = 90
@@ -85,10 +85,10 @@ locals {
 
     "image-pull-backoff" = {
       message = <<-EOF
-      Pod {{pod_name.name}} is in ImagePullBackOff on {{kube_namespace.name}} on {{kube_cluster_name.name}} cluster.
+      Pod {{pod_name.name}} is in ImagePullBackOff on {{kube_namespace.name}} on cluster: {{kube_cluster_name.name}}.
       EOF
 
-      name                = "[Kubernetes] Pods in ImagePullBackOff on namespace {{kube_namespace.name}} on ${local.kubernetes_cluster_name}"
+      name                = "[Kubernetes] Pods in ImagePullBackOff - namespace: {{kube_namespace.name}} cluster ${local.kubernetes_cluster_name}"
       priority            = 3
       query               = "max(last_10m):default_zero(max:kubernetes_state.container.status_report.count.waiting{reason:imagepullbackoff, kube_cluster_name:${local.kubernetes_cluster_name}} by {kube_namespace,pod_name}) >= 1"
       thresholds_critical = 1
@@ -98,10 +98,10 @@ locals {
 
     "restarting-pods" = {
       message = <<-EOF
-      Pod {{pod_name.name}} restarted multiple times in the last five minutes on {{kube_cluster_name.name}} cluster.
+      Pod {{pod_name.name}} restarted multiple times in the last five minutes on {{kube_namespace.name}} on cluster: {{kube_cluster_name.name}}.
       EOF
 
-      name                = "[Kubernetes] Pods Restarting on ${local.kubernetes_cluster_name}"
+      name                = "[Kubernetes] Pods Restarting - cluster: ${local.kubernetes_cluster_name}"
       priority            = 3
       query               = "change(max(last_5m),last_5m):sum:kubernetes.containers.restarts{kube_cluster_name:${local.kubernetes_cluster_name}} by {pod_name} > 5"
       thresholds_critical = 5
@@ -111,10 +111,10 @@ locals {
 
     "unschedulable-node" = {
       message = <<-EOF
-      More than 20% of nodes are unschedulable on {{kube_cluster_name.name}} cluster.
+      More than 20% of nodes are unschedulable on cluster: {{kube_cluster_name.name}}.
       EOF
 
-      name                = "[Kubernetes] Unschedulable Nodes on ${local.kubernetes_cluster_name}"
+      name                = "[Kubernetes] Unschedulable Nodes - cluster: ${local.kubernetes_cluster_name}"
       priority            = 3
       query               = "max(last_15m):default_zero(sum:kubernetes_state.node.status{status:schedulable}, kube_cluster_name:${local.kubernetes_cluster_name}} * 100 / sum:kubernetes_state.node.status{*} by kube_cluster_name:${local.kubernetes_cluster_name}} < 80"
       thresholds_critical = 80
