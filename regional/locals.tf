3e1a421b103f0f45c3223a09d78187b73bfd94a5
Brett Curtis <brett@osinfra.io>
2024-10-13 12:20:43 -0400

    Fix regex handling for environment, region, and zone

--- regional/locals.tf
+++ regional/locals.tf
@@ -15,7 +15,7 @@ locals {
   environment = (
     terraform.workspace == "default" ?
     "mock-environment" :
-    (regex(".*-(?P<environment>[^-]+)$", terraform.workspace)["environment"])
+    regex(".*-(?P<environment>[^-]+)$", terraform.workspace)["environment"]
   )
 
   helm_sensitive_values = {
@@ -38,12 +38,17 @@ locals {
   region = (
     terraform.workspace == "default" ?
     "mock-region" :
-    (regex("^(?P<region>[^-]+-[^-]+)", terraform.workspace)["region"])
+    regex("^(?P<region>[^-]+-[^-]+)", terraform.workspace)["region"]
   )
+
 
   zone = (
     terraform.workspace == "default" ?
     "mock-zone" :
-    (regex("^(?P<region>[^-]+-[^-]+)-(?P<zone>[^-]+)", terraform.workspace)["zone"])
+    (
+      regex("^(?P<region>[^-]+-[^-]+)(?:-(?P<zone>[^-]+))?-.*$", terraform.workspace)["zone"] != "" ?
+      regex("^(?P<region>[^-]+-[^-]+)(?:-(?P<zone>[^-]+))?-.*$", terraform.workspace)["zone"] :
+      null
+    )
   )
 }
