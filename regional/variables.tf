3e5fcdc76dcc4fcd4b8729e4199d1508549a29c0
Brett Curtis <brett@osinfra.io>
2025-01-25 07:38:06 -0500

    Update Datadog Operator and Node Agent versions to 2.5.1 and 7.61.0, respectively; bump pre-commit hooks for Terraform and Checkov

--- regional/variables.tf
+++ regional/variables.tf
@@ -39,7 +39,7 @@ variable "limits_memory" {
 variable "operator_version" {
   description = "The version of the Datadog Operator to install"
   type        = string
-  default     = "2.5.0"
+  default     = "2.5.1"
 }
 
 variable "requests_cpu" {
